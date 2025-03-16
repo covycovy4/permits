1e6d8bb02eb7a4d3e79b7f1676d376f8b898d367
covenantchiwasa@gmail.com <covenantchiwasa@gmail.com>
2025-03-16 12:31:32 +0200

    Fix CORS initialization

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import os
 from app import create_app, db
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 # Create the app using the factory function
 app = create_app()
 
+# Enable CORS (Cross-Origin Resource Sharing)
+CORS(app)
+
 # Initialize Flask-Migrate with the app and db
 migrate = Migrate(app, db)
-
-# The rest of your app setup follows
 
 if __name__ == "__main__":
     # Running the Flask app in development mode
